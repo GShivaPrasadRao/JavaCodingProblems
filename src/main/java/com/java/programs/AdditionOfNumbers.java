c3316db576288d7881ce574fe1c926c972ba9bf0
GShivaPrasadRao <gshivaprasad333@gmail.com>
2022-09-01 17:28:52 +0530

    Program commit

---
+++ src/main/java/com/java/programs/AdditionOfNumbers.java
@@ -0,0 +1,11 @@
+package com.java.programs;
+
+public class AdditionOfNumbers {
+
+    public static void main(String[] args)
+    {
+        int a = 10 ,b=20,sum;
+        sum = a + b;
+        System.out.println("Sum="+sum);
+    }
+}
